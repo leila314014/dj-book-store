d6de57982a4e1ce1966ae6184e082064f30ccbb2
linda314014 <l940026l@gmail.com>
2020-02-04 21:30:10 +0800

    django0204

---
+++ books/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Book
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name', 'description')
+    list_filter = ('is_online',)
